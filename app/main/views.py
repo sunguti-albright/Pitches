e26381702e5d2b27a638d237c6db44dfa1c018b6
albright.human <sungutialbright@gmail.com>
2022-05-09 20:10:12 +0300

    add route for new pitch

--- app/main/views.py
+++ app/main/views.py
@@ -21,23 +21,23 @@ def index():
 
     return render_template('home.html',title=title,pitch=pitch,upvotes=upvotes,business_pitch=business_pitch,interview_pitch=interview_pitch,sales_pitch=sales_pitch,production_pitch=promotion_pitch)
 
-# @main.route('/pitches/new/',methods=['GET','POST'])
-# @login_required
-# def new_pitch():
-#     form = PitchNow()
-#     my_upvotes = Upvote.query.filter_by(pitch_id=Pitch.id)
-#     if form.validate_on_submit():
-#         description = form.description.data
-#         title = form.title.data
-#         owner_id = current_user
-#         category = form.category.data
-#         print(current_user.get_current_object().id)
-#         new_pitch = Pitch(owner_id =current_user._get_current_object().id, title = title,description=description,category=category)
-#         db.session.add(new_pitch)
-#         db.session.commit()
+@main.route('/pitches/new/',methods=['GET','POST'])
+@login_required
+def new_pitch():
+    form = PitchNow()
+    my_upvotes = Upvote.query.filter_by(pitch_id=Pitch.id)
+    if form.validate_on_submit():
+        description = form.description.data
+        title = form.title.data
+        owner_id = current_user
+        category = form.category.data
+        print(current_user.get_current_object().id)
+        new_pitch = Pitch(owner_id =current_user._get_current_object().id, title = title,description=description,category=category)
+        db.session.add(new_pitch)
+        db.session.commit()
 
-#         return redirect(url_for('main.index'))
-#     return render_template('pitches.html',form=form)
+        return redirect(url_for('main.index'))
+    return render_template('pitches.html',form=form)
 
 # @main.route('/comment/new/<int:pitch_id>',methods=['GET','POST'])
 # @login_required
