15dc0d10d57b9317a04f215847dfdb01e8a48323
albright.human <sungutialbright@gmail.com>
2022-05-09 20:17:04 +0300

    this commit adds route for updating user profile

--- app/main/views.py
+++ app/main/views.py
@@ -91,33 +91,33 @@ def profile(uname):
         abort(404)
     return render_template("profile/profiles.html",user=user)
 
-# @main.route('/user/<uname>/update',methods=['GET','POST'])
-# @login_required
-# def update_profile(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if user is None:
-#         abort(404)
+@main.route('/user/<uname>/update',methods=['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
 
-#     form = UpdateProfile()
+    form = UpdateProfile()
 
-#     if form.validate_on_submit():
-#         user.bio = form.bio.data
+    if form.validate_on_submit():
+        user.bio = form.bio.data
 
-#         db.session.add(user)
-#         db.session.commit()
+        db.session.add(user)
+        db.session.commit()
 
-#         return redirect(url_for('.profile',uname=user.username))
+        return redirect(url_for('.profile',uname=user.username))
 
-#     return render_template('profile/update.html',form=form)
+    return render_template('profile/update.html',form=form)
 
-# @main.route('/user/<uname>/update/pic',methods=['GET','POST'])
-# @login_required
-# def update_pic(uname):
-#     user = User.query.filter_by(username=uname).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path 
-#         db.session.commit()
+@main.route('/user/<uname>/update/pic',methods=['GET','POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path 
+        db.session.commit()
 
-#     return redirect(url_for('main.profile',uname=uname))
+    return redirect(url_for('main.profile',uname=uname))
